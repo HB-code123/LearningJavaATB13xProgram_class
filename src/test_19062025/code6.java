fac19899ef24d5954b7a651f4b8af030b6da476a
HB-code123 <harpreetbhatia92@gmail.com>
2025-06-18 21:55:27 -0500

    commit

---
+++ src/test_19062025/code6.java
@@ -0,0 +1,18 @@
+package test_19062025;
+
+import java.util.Scanner;
+
+public class code6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter any number :  ");
+        int num1 = scanner.nextInt();
+        if (num1 > 0){
+            System.out.println("Positive");
+            else
+            {
+                System.out.println("Negitive");
+        }
+    }
+}
